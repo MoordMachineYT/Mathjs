9833e5424fa51f1c1f72bd7eab5169f999b18e7e
MoordMachineYT <33372542+MoordMachineYT@users.noreply.github.com>
2018-01-23 11:46:01 +0100

    Update factorial.js

--- functions/factorial.js
+++ functions/factorial.js
@@ -6,7 +6,7 @@
 */
 function factorial (number) {
   var newNumber = 1;
-  for (i = number; i > 0; i--) {
+  for (var i = number; i > 0; i--) {
     newNumber = newNumber * i;
   }
   return newNumber;
