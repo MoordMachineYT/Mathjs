4c1b329f2ff5dcfe8d0f272101cc7e5cc73d64b0
MoordMachineYT <33372542+MoordMachineYT@users.noreply.github.com>
2018-01-23 16:53:14 +0100

    Exported lcm
    Exported lcm

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = {
   double: require ("./functions/double.js"),
   factorial: require ("./functions/factorial.js"),
   gcd: require ("./functions/gcd.js"),
+  lcm: require ("./functions/lcm.js"),
   multiply: require ("./functions/multiply.js"),
   triple: require ("./functions/triple.js")
 }
